598e93fe0692899ab3e7ebaed996b0deb0fecbe0
Doomfangre <Doomfangre@gmail.com>
2026-02-27 09:55:29 -0800

    ASYNC Stuff
    Tried to add functions that call a list using JSON. Tried to make a function that calculated expenses. Added some CSS. Changed the part 2 input with the Part one. Added ID's to part 1.

---
+++ script.js
@@ -0,0 +1,11 @@
+async function getCareers() {
+    let response = await fetch('https://eecu-data-server.vercel.app/data');
+    let careers = await response.json();
+}
+// Tried to use fetchAPI to get the careers into an array.
+// Needs ASYNC, don't delete
+function monthly(...items) {
+    const taxes = [document.getElementById(expenseInput1), document.getElementById(expenseInput2), document.getElementById(expenseInput3), document.getElementById(expenseInput4)];
+    
+}
+// Function for calculating Expenses.
